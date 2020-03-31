ba794e5915c822531af6051f3b89f7fc2298dfc2
Joe Reid <joe@joereid.com>
2020-03-31 09:31:12 -0400

    oops, forgot to add write.c. It contains code for writing a demo file

---
+++ write.c
@@ -0,0 +1,60 @@
+#include "demo.h"
+
+/**
+ * Write a new demo file from the demo_s structure
+ */
+void WriteDemoFile(const char *filename)
+{
+	FILE *fp;
+	uint32_t i;
+	msg_buffer_t chunklen;
+
+	fp = fopen(filename, "wb");
+	if (!fp) {
+		return;
+	}
+
+	memset(&msg2, 0, sizeof(msg_buffer_t));
+
+	// serverdata first
+	MSG_WriteByte(svc_serverdata);
+	MSG_WriteLong(demo.serverdata.version);
+	MSG_WriteLong(demo.serverdata.count);
+	MSG_WriteByte(demo.serverdata.demo);
+	MSG_WriteString(demo.serverdata.gamedir);
+	MSG_WriteShort(demo.serverdata.client_edict);
+	MSG_WriteString(demo.serverdata.map);
+
+	// then configstrings
+	for (i=0; i<MAX_CONFIGSTRINGS; i++) {
+		if (demo.configstrings[i].index != i) {
+			continue;
+		}
+
+		MSG_WriteByte(svc_configstring);
+		MSG_WriteShort(demo.configstrings[i].index);
+		MSG_WriteString(demo.configstrings[i].string);
+	}
+
+	/*
+	// then baselines
+	for (i=0; i<MAX_EDICTS; i++) {
+		if (demo.baselines[i].number != i) {
+			continue;
+		}
+
+		MSG_WriteByte(svc_spawnbaseline);
+		MSG_WriteShort(demo.configstrings[i].index);
+		MSG_WriteString(demo.configstrings[i].string);
+	}
+	*/
+
+	MSG_ChunkLength(msg2.length, &chunklen);
+	fwrite(&chunklen, 1, sizeof(uint32_t), fp);
+	printf("len: %d\n", msg2.length);
+	fwrite(&msg2, 1, msg2.length, fp);
+
+
+	fclose(fp);
+}
+
